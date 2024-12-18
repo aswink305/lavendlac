d724bae71463e57c2cf73e226a0ac7dad36b330e
aswin <aswinkasaas@gmail.com>
2024-12-18 21:34:18 +0530

    re update

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Landing = () => {
     return (
         <>
-            <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+            <div className="min-h-screen bg-gray-100 p-[20px] flex items-center justify-center">
                 <div >
                     <div className='flex ease-in  justify-center w-[100%]'>
-                        <img src="/Images/MainLogo.png" className='h-[300px]' alt="" />
+                        <img src="/Images/MainLogo.png" className=' max-md:h-[200px] max-sm:h-[200px] h-[300px]' alt="" />
                     </div>
-                    <h1 className="text-5xl ease-in font-dancing text-blue-500">
+                    <h1 className="  max-md:text-4xl text-5xl text-center ease-in font-dancing text-blue-500">
                         Welcome to Lavendlac Designs !
                     </h1>
                 </div>
