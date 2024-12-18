7c50adcc0edd699ce3b17561a652cca6cbebfa6e
aswin <aswinkasaas@gmail.com>
2024-12-18 20:47:46 +0530

    updating landing page

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -5,7 +5,7 @@ const Landing = () => {
         <>
             <div className="min-h-screen bg-gray-100 flex items-center justify-center">
                 <h1 className="text-4xl font-bold text-blue-500">
-                    Tailwind CSS is set up !
+                    Successfully git pushed
                 </h1>
             </div>
         </>
