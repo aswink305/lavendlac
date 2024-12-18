bfa0499e9b0f651a3feadab38facbe7535dd3dc2
aswin <aswinkasaas@gmail.com>
2024-12-18 21:01:29 +0530

    update

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -5,7 +5,7 @@ const Landing = () => {
         <>
             <div className="min-h-screen bg-gray-100 flex items-center justify-center">
                 <h1 className="text-4xl font-bold text-blue-500">
-                    Successfully git pushed
+                    Successfully git pushed !
                 </h1>
             </div>
         </>
